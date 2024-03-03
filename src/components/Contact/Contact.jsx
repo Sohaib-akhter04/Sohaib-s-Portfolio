afc88e32fb0efb38da2286587cbb49cee96b5555
Sohaib akhter <k200292@nu.edu.pk>
2024-03-03 10:17:49 +0500

    changing data

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ function Contact  () {
           <ul className={styles.links}>
             <li className={styles.link}>
               <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-              <a href="mailto:myemail@email.com">myemail@email.com</a>
+              <a href="mailto:akhtersohaib56@gmail.com">akhtersohaib56@gmail.com</a>
             </li>
             <li className={styles.link}>
               <img
                 src={getImageUrl("contact/linkedinIcon.png")}
                 alt="LinkedIn icon"
               />
-              <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+              <a href="www.linkedin.com/in/sohaib-akhter04">linkedin.com/in/sohaib-akhter04/</a>
             </li>
             <li className={styles.link}>
               <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-              <a href="https://www.github.com/myname">github.com/myname</a>
+              <a href="https://github.com/Sohaib-akhter04">github.com/Sohaib-akhter04</a>
             </li>
           </ul>
         </footer>
